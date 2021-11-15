3e4ca6d39f4eb8045e5825e4a201dfb93025130c
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2021-11-15 04:25:48 +0200

    update ...
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- MAINTAINERS.c
+++ MAINTAINERS.c
@@ -1,4 +1,3 @@
-/*
 PERFORMANCE EVENTS SUBSYSTEM
 M:	Peter Zijlstra <peterz@infradead.org>
 M:	Ingo Molnar <mingo@redhat.com>
@@ -11,17 +10,16 @@ L:	linux-perf-users@vger.kernel.org
 L:	linux-kernel@vger.kernel.org
 S:	Supported
 W:	https://perf.wiki.kernel.org/
-T:	git git://git.kernel.org/pub/scm/linux/kernel/git/tip/tip.git perf/core
-F:	arch/*/events/*
-F:	arch/*/events/*/*
-F:	arch/*/include/asm/perf_event.h
-F:	arch/*/kernel/*/*/perf_event*.c
+T:	git git://git.kernel.org/pub/scm/linux/kernel/git/tip/tip.git perf/core */
+F:	arch/*/events/* */
+F:	arch/*/events/*/* */
+F:	arch/*/include/asm/perf_event.h */
+F:	arch/*/kernel/*/*/perf_event*.c */
 F:	arch/*/kernel/*/perf_event*.c
-F:	arch/*/kernel/perf_callchain.c
-F:	arch/*/kernel/perf_event*.c
+F:	arch/*/kernel/perf_callchain.c */
+F:	arch/*/kernel/perf_event*.c */
 F:	include/linux/perf_event.h
 F:	include/uapi/linux/perf_event.h
-F:	kernel/events/* TODO
+F:	kernel/events/* */ TODO
 F:	tools/lib/perf/ DONE
 F:	tools/perf/ DONE
-*/
